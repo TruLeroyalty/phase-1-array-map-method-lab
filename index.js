12aa1944ba5b3806fcd2cd24502f7c4988bdb0c5
Leroy Scott Jr <leroysjr@yahoo.com>
2023-04-02 19:43:41 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = function() {
+  return tutorials.map((blam) => {
+    return blam.split(" ").map((words) => {
+      return (words.charAt(0).toUpperCase() + words.slice(1));
+    }).join(" ");  
+    })}
+
+console.log(titleCased(tutorials))
